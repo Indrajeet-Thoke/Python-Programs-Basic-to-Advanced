0013dbb4783e91d867bcb0b551dd49ec1c691ac3
Indrajeet Thoke <ijthoke2021@gmail.com>
2025-09-18 21:29:48 -0700

    Add files via upload

---
+++ even_odd.py
@@ -0,0 +1,7 @@
+num = int(input("Enter a number to check whether the number is Even or Odd"))
+if num % 2==0:
+    print("NUMBER IS EVEN")
+elif num %2 !=0:
+    print("NUMBER IS ODD")
+else:
+    print("you are not human")
